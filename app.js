0fdc0fa5bb459b3fa2c10d63e7c2ad5468055ec7
taeate <yes3144@nate.com>
2022-08-26 22:45:48 +0900

    addEventListener 이용하여 마우스 호버시 컬러바꿈 호버아웃시 블랙

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 const title = document.querySelector(".hello h1"); // <h1>Grab me!</h1>
 
 function handleClick(){
+  title.style.color = "red";
+}
+
+function handleMouseEnter(){
   title.style.color = "blue";
 }
 
+function handleMouseOut(){
+  title.style.color = "black";
+}
+
 title.addEventListener("click", handleClick);
+title.addEventListener("mouseenter", handleMouseEnter)
+title.addEventListener("mouseout", handleMouseOut)
 
 console.log(title);
 
