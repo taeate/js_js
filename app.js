9bb4f31a5ecbad3d7a17ea67307e90391625b7b7
taeate <yes3144@nate.com>
2022-08-26 22:29:26 +0900

    addEventListener 이용하여 클릭시 함수실행

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 
 const title = document.querySelector(".hello h1"); // <h1>Grab me!</h1>
 
-title.style.color = "blue";
+function handleClick(){
+  console.log("title was clicked!");
+}
+
+title.addEventListener("click", handleClick);
 
 console.log(title);
 
