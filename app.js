d6493da89c43701a74ed5084e55f03f3a4d53494
taeate <yes3144@nate.com>
2022-08-29 13:44:56 +0900

    html 에서 유효성 검사 시키기

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const loginbutton = document.querySelector('#login-form button');
 
 function onLoginBtnClick() {
   const username = loginInput.value;
-  if(username === ""){
-    alert("이름을 입력해주세요.");
-  } else if(username.length > 15){
-    alert("너무 길어요.");
-  } 
+  
 }
 loginbutton.addEventListener('click', onLoginBtnClick);
