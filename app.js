ec036a3962d75f828d9d0c8319e51b05011b46da
taeate <yes3144@nate.com>
2022-08-29 14:44:47 +0900

    주석추가

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const gretting = document.querySelector('#gretting');
 const HIDDEN_CLASSNAME = 'hidden';
 
 function onLoginSubmit(event) {
-  event.preventDefault();
-  const username = loginInput.value;
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  gretting.innerText = `안녕하세요 ${username} 님 반갑습니다.`;
+  event.preventDefault(); //기본 동작 멈춤
+  loginForm.classList.add(HIDDEN_CLASSNAME); // form에 hidden class 추가
+  const username = loginInput.value; // loginInput 값을 변수에 저장
+  gretting.innerText = `안녕하세요 ${username} 님 반갑습니다.`; 
   gretting.classList.remove(HIDDEN_CLASSNAME);
   
 }
-
 
 loginForm.addEventListener('submit', onLoginSubmit);
