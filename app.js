cd2db48b143e9c9d42d388826bc0b39524bf1719
taeate <yes3144@nate.com>
2022-08-29 09:55:58 +0900

    if/else 사용

--- app.js
+++ app.js
@@ -2,43 +2,21 @@
 const h1 = document.querySelector(".hello h1"); // <h1>Grab me!</h1>
 
 function handleClick(){
-  h1.style.color = "red";
-}
 
-function handleMouseEnter(){
-  h1.style.color = "blue";
-}
+  const currentColor = h1.style.color;
+  let newColor;
 
-function handleMouseOut(){
-  h1.style.color = "black";
-}
-
-function handleResize(){
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleCopy(){
-  alert("복사할수없습니다!");
-}
-
-function handleoffline(){
-  alert("SOS no WIFI!");
-}
-
-function handleonline(){
-  alert("SOS ONLINE!");
-
+  if(currentColor === "red"){
+    newColor = "blue";
+  }else{
+    newColor = "red";
+  }
+  h1.style.color = newColor;
 }
 
 
-h1.onclick = handleClick;
-h1.addEventListener("mouseenter", handleMouseEnter)
-h1.addEventListener("mouseout", handleMouseOut)
+h1.addEventListener('click', handleClick);
 
-window.addEventListener("resize", handleResize);
-window.addEventListener("copy", handleCopy);
-window.addEventListener("offline", handleoffline);
-window.addEventListener("online", handleonline);
 
 
 
