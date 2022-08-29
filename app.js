e8582db0b04aac5121ae1d9c3b998c967dbc1b5b
taeate <yes3144@nate.com>
2022-08-29 10:06:01 +0900

    class 추가 제거 5줄을 1줄코드로 toggle

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const h1 = document.querySelector(".hello h1"); // <h1>Grab me!</h1>
 function handleClick(){
 
   const clickedClass = "clicked";
-
-  if(h1.classList.contains(clickedClass)){
-    h1.classList.remove(clickedClass);
-  }else{
-    h1.classList.add(clickedClass);
-  }
+  h1.classList.toggle("clicked");
 }
 
  
