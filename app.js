1ce88fdf78bbdb319cbe389497df1189673f6de0
taeate <yes3144@nate.com>
2022-08-29 15:15:17 +0900

    username localstorage 에 저장

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ function onLoginSubmit(event) {
   event.preventDefault(); //기본 동작 멈춤
   loginForm.classList.add(HIDDEN_CLASSNAME); // form에 hidden class 추가
   const username = loginInput.value; // loginInput 값을 변수에 저장
+  localStorage.setItem("username", username);
   gretting.innerText = `안녕하세요 ${username} 님 반갑습니다.`; 
   gretting.classList.remove(HIDDEN_CLASSNAME);
-  
 }
 
 loginForm.addEventListener('submit', onLoginSubmit);
