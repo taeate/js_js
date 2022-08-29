f3853e0712c797dc1666b7239f708ebdad15e7a0
taeate <yes3144@nate.com>
2022-08-29 14:29:23 +0900

    input > sumit 하면 인풋창 hidden , h1 생김

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const gretting = document.querySelector('#gretting');
 
+const HIDDEN_CLASSNAME = 'hidden';
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
-  console.log(event);
-
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  gretting.innerText = `안녕하세요 ${username} 님 반갑습니다.`;
+  gretting.classList.remove(HIDDEN_CLASSNAME);
+  
 }
+
 
 loginForm.addEventListener('submit', onLoginSubmit);
