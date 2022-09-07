0f2ffe6ef5a59b6cd2ae2148df3fdda1b6d13ed2
taeate <yes3144@nate.com>
2022-09-07 17:07:52 +0900

    localstorge 에 리스트로 저장

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById('todo-form');
 const toDoinput = document.querySelector('#todo-form input');
 const toDolist = document.getElementById('todo-list');
 
+const toDos = [];
+
+function saveToDos(){
+  localStorage.setItem('todos', JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -23,7 +29,9 @@ function handleToDoSubmit(event){
   event.preventDefault();
   const newTodo = toDoinput.value;
   toDoinput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
